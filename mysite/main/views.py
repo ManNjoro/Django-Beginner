53f8c188bd391d3fe0486534d28de30db1141826
Man Njoro <elijohnmwoho@gmail.com>
2024-04-03 23:02:12 +0300

    URL pattern and view function changed to display ToDoList name by id
    * urls.py: Changed path pattern for root URL to accept integer argument, changed name of "v1" path
    * views.py: Added import for ToDoList and Item models, created index view function that accepts an integer id argument and retrieves the corresponding ToDoList object to display its name using HttpResponse.

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models  import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Man Njoro</h1>")
-
-def v1(response):
-    return HttpResponse("<h1>view 1!</h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return HttpResponse(f"<h1>{ls.name}</h1>")
