2535c88263475a49ab90ef98d32ce2dfcbbd8aa7
Man Njoro <elijohnmwoho@gmail.com>
2024-04-04 20:41:36 +0300

    "Added title and content blocks to base template, set title and content for home page, updated views to render list template for index view and pass a context variable to home view"

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,7 +7,7 @@ from .models  import ToDoList, Item
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
     item = ls.item_set.get(id=id)
-    return render(response, "main/base.html", {})
+    return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
-    return render(response, "main/home.html", {})
+    return render(response, "main/home.html", {"name": "Test"})
