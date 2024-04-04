5b714b3bc9a57a333cc1177b26cf6db2e748df50
Man Njoro <elijohnmwoho@gmail.com>
2024-04-04 22:11:04 +0300

    "Added handling for POST request in index view"
    This commit adds a few lines to the index view function in the views.py file. Now, if the request method is POST, it checks if the "save" button was clicked in the form. Currently, there is no functionality associated with the "save" button, so this code just passes when the "save" button is clicked. This change allows for the addition of functionality when the form is submitted in the future.

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,6 +7,9 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+    if response.method == "POST":
+        if response.POST.get("save"):
+            pass
     return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
