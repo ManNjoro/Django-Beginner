2f27ebd611f563746176c00c572b3f7757f0ca4e
Man Njoro <elijohnmwoho@gmail.com>
2024-04-08 22:37:06 +0300

    "Update views.py: add user's to-do list check for index view and refactor code"
    This commit updates the views.py file by adding a check for the user's to-do list in the index view and refactoring the code for handling POST requests. The changes include:
    
    * Checking if the list (ls) is in the user's to-do list before processing the POST request in the index view.
    * Refactoring the code for handling the "save" action in the POST request for the index view.
    * Refactoring the code for handling the "newItem" action in the POST request for the index view, including adding a check for invalid input.
    * Changing the template rendered for the index view from "main/list.html" to "main/view.html".

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -7,22 +7,25 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
-    if response.method == "POST":
-        if response.POST.get("save"):
-            for item in ls.item_set.all():
-                if response.POST.get("c" + str(item.id)) == "clicked":
-                    item.complete = True
-                else:
-                    item.complete = False
-                item.save()
-        elif response.POST.get("newItem"):
-            txt  = response.POST.get("new")
+    if ls in response.user.todolist.all():
 
-            if len(txt) > 2:
-                ls.item_set.create(text=txt, complete=False)
-            else:
-                print("invalid")
-    return render(response, "main/list.html", {"ls": ls})
+        if response.method == "POST":
+            if response.POST.get("save"):
+                for item in ls.item_set.all():
+                    if response.POST.get("c" + str(item.id)) == "clicked":
+                        item.complete = True
+                    else:
+                        item.complete = False
+                    item.save()
+            elif response.POST.get("newItem"):
+                txt  = response.POST.get("new")
+
+                if len(txt) > 2:
+                    ls.item_set.create(text=txt, complete=False)
+                else:
+                    print("invalid")
+        return render(response, "main/list.html", {"ls": ls})
+    return render(response, "main/view.html", {"ls": ls})
 
 def home(response):
     return render(response, "main/home.html", {})
